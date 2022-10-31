fed3b30ba297faa5481f0372b35e89408059132f
Mükremin Çetinkaya <79249386+mkcetinkaya@users.noreply.github.com>
2022-10-31 10:47:53 +0300

    Update config.js

--- packages/dashboard-e2e/scripts/config.js
+++ packages/dashboard-e2e/scripts/config.js
@@ -2,7 +2,7 @@ process.env.BUILD_PATH = process.env.BUILD_PATH || '../dashboard-e2e/build';
 process.env.REACT_APP_TRAJECTORY_SERVER =
   process.env.REACT_APP_TRAJECTORY_SERVER || 'ws://157.90.235.4:31086';
 process.env.REACT_APP_RMF_SERVER = process.env.REACT_APP_RMF_SERVER || 'http://157.90.235.4:31080';
-process.env.E2E_DASHBOARD_URL = process.env.E2E_DASHBOARD_URL || 'http://localhost:5000';
+process.env.E2E_DASHBOARD_URL = process.env.E2E_DASHBOARD_URL || '157.90.235.4:31050';
 
 const services = [];
 // eslint-disable-next-line no-eval
