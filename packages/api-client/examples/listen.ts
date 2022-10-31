2b2782f8a0f238233e8cc93d04a6b51a3d733e7a
Mükremin Çetinkaya <79249386+mkcetinkaya@users.noreply.github.com>
2022-10-31 10:45:01 +0300

    Update listen.ts

--- packages/api-client/examples/listen.ts
+++ packages/api-client/examples/listen.ts
@@ -1,6 +1,6 @@
 import { SioClient } from '../lib';
 
-const client = new SioClient('http://localhost:8000');
+const client = new SioClient('http://157.90.235.4:31080');
 const l1 = client.subscribeDoorState('coe_door', console.log);
 const l2 = client.subscribeDoorState('coe_door', console.log);
 
