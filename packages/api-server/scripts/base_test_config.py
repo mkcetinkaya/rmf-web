45047e7cc09abe369a92ca5b0c7d67cfeb6319cf
Mükremin Çetinkaya <79249386+mkcetinkaya@users.noreply.github.com>
2022-10-31 10:45:43 +0300

    Update base_test_config.py

--- packages/api-server/scripts/base_test_config.py
+++ packages/api-server/scripts/base_test_config.py
@@ -4,10 +4,10 @@ from api_server.default_config import config
 
 here = os.path.dirname(__file__)
 
-test_port = os.environ.get("RMF_SERVER_TEST_PORT", "8000")
+test_port = os.environ.get("RMF_SERVER_TEST_PORT", "31080")
 config.update(
     {
-        "host": "127.0.0.1",
+        "host": "157.90.235.4",
         "port": int(test_port),
         "log_level": "CRITICAL",
         "jwt_public_key": f"{here}/test.pub",
